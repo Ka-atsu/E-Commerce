ed5ad9746d6249eb09d992e02459afe86642bc70
Everlyn Tisado <tisadoeverlyn42@gmail.com>
2024-10-22 16:37:02 +0800

    Added a Submit Spinner in Edit Product Component

--- my-react-app/src/Components/Product Management/EditProductComponent.jsx
+++ my-react-app/src/Components/Product Management/EditProductComponent.jsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Barcode from 'react-barcode';
+import Spinner from 'react-bootstrap/Spinner'; // Import Spinner from Bootstrap
 
 const EditProductComponent = ({ productId }) => {
     const [viewProduct, setViewProduct] = useState([]);
@@ -13,24 +14,26 @@ const EditProductComponent = ({ productId }) => {
     const [productAvailableQuantity, setProductAvailableQuantity] = useState(viewProduct.product_available_amount);
     const [validatedForm, setValidatedForm] = useState(false);
     const [validationErrors, setValidationErrors] = useState({});
+    const [loading, setLoading] = useState(false); // State for tracking submission status
     const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`http://127.0.0.1:8000/api/view_product/${productId}`)
-          .then(response => response.json())
-          .then(data => {
+            .then(response => response.json())
+            .then(data => {
                 setViewProduct(data);
                 setProductName(data.product_name);
                 setProductDescription(data.product_description);
                 setProductPrice(data.product_amount);
                 setProductAvailableQuantity(data.product_available_quantity);
             })
-          .catch(error => console.error('Error fetching tasks:', error));
-    }, []);
+            .catch(error => console.error('Error fetching tasks:', error));
+    }, [productId]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setValidatedForm(true);
+        setLoading(true); // Set loading to true when form submission starts
 
         const data = {
             product_name: productName,
@@ -40,23 +43,25 @@ const EditProductComponent = ({ productId }) => {
         };
 
         axios.put('http://127.0.0.1:8000/api/update_product/' + productId, data)
-                .then(response => {
-                    navigate('/dashboard');
-                })
-                .catch(error => {
-                    if(error.response && error.response.status === 422) {
-                        const backEndErrors = error.response.data.validationErrors;
-                        setValidationErrors(backEndErrors);
-                    } else {
-                        console.error('Error updating product: ', error);
-                    }
-                });
+            .then(response => {
+                setLoading(false); // Set loading to false after success
+                navigate('/dashboard');
+            })
+            .catch(error => {
+                setLoading(false); // Set loading to false if an error occurs
+                if (error.response && error.response.status === 422) {
+                    const backEndErrors = error.response.data.validationErrors;
+                    setValidationErrors(backEndErrors);
+                } else {
+                    console.error('Error updating product: ', error);
+                }
+            });
     }
 
     const handleInputChange = (setter, errorName) => (event) => {
         setter(event.target.value);
-        if(validatedForm)
-            setValidationErrors((prev) => ({...prev, [errorName]: null}));
+        if (validatedForm)
+            setValidationErrors((prev) => ({ ...prev, [errorName]: null }));
     }
 
     return (
@@ -66,15 +71,15 @@ const EditProductComponent = ({ productId }) => {
                 <Form noValidate validated={validatedForm} onSubmit={handleSubmit}>
                     <Form.Group>
                         <Form.Label>Barcode:
-                        <Barcode value={viewProduct.product_barcode}  width={2} height={50}/></Form.Label>
+                        <Barcode value={viewProduct.product_barcode} width={2} height={50} /></Form.Label>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Name</Form.Label>
-                        <Form.Control 
-                            type='text' 
-                            placeholder={viewProduct.product_name} 
-                            value={productName} 
-                            onChange={handleInputChange(setProductName, 'product_name')} 
+                        <Form.Control
+                            type='text'
+                            placeholder={viewProduct.product_name}
+                            value={productName}
+                            onChange={handleInputChange(setProductName, 'product_name')}
                             isInvalid={!!validationErrors.product_name}
                             required
                         />
@@ -82,24 +87,24 @@ const EditProductComponent = ({ productId }) => {
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Description</Form.Label>
-                        <Form.Control 
-                            as='textarea' 
-                            placeholder={viewProduct.product_description} 
-                            value={productDescription} 
-                            onChange={handleInputChange(setProductDescription, 'product_description')} 
+                        <Form.Control
+                            as='textarea'
+                            placeholder={viewProduct.product_description}
+                            value={productDescription}
+                            onChange={handleInputChange(setProductDescription, 'product_description')}
                             isInvalid={!!validationErrors.product_description}
                             required
                         />
-                        <Form.Control.Feedback type="invalid">{validationErrors.product_description ? validationErrors.product_description[0] : null}</Form.Control.Feedback>    
+                        <Form.Control.Feedback type="invalid">{validationErrors.product_description ? validationErrors.product_description[0] : null}</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Price</Form.Label>
-                        <Form.Control 
-                            type='number' 
-                            step='any' 
-                            placeholder={viewProduct.product_amount} 
-                            value={productPrice} 
-                            onChange={handleInputChange(setProductPrice, 'product_amount')} 
+                        <Form.Control
+                            type='number'
+                            step='any'
+                            placeholder={viewProduct.product_amount}
+                            value={productPrice}
+                            onChange={handleInputChange(setProductPrice, 'product_amount')}
                             isInvalid={!!validationErrors.product_amount}
                             required
                         />
@@ -107,18 +112,24 @@ const EditProductComponent = ({ productId }) => {
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Quantity</Form.Label>
-                        <Form.Control 
-                            type='number' 
-                            placeholder={viewProduct.product_available_quantity} 
-                            value={productAvailableQuantity} 
-                            onChange={handleInputChange(setProductAvailableQuantity, 'product_available_quantity')} 
+                        <Form.Control
+                            type='number'
+                            placeholder={viewProduct.product_available_quantity}
+                            value={productAvailableQuantity}
+                            onChange={handleInputChange(setProductAvailableQuantity, 'product_available_quantity')}
                             isInvalid={!!validationErrors.product_available_quantity}
                             required
                         />
                         <Form.Control.Feedback type="invalid">{validationErrors.product_available_quantity ? validationErrors.product_available_quantity[0] : null}</Form.Control.Feedback>
                     </Form.Group>
                     <div className="submitContainer">
-                        <button className="btn btn-primary" type='submit'>Submit</button>
+                        <button className="btn btn-primary" type='submit' disabled={loading}>
+                            {loading ? (
+                                <>
+                                    <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" /> ...
+                                </>
+                            ) : 'Submit'}
+                        </button>
                         <Link to="/dashboard" className="btn btn-secondary" role="button">Cancel</Link>
                     </div>
                 </Form>
